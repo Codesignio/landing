1710c0db2ac88b6ff09196ecff189060f14f9d25
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-12-03 14:44:59 +0100

    chrome.codesign.io

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/landing/"
 css_dir = "static/stylesheets"
 sass_dir = "static/sass"
 images_dir = "static/images"
